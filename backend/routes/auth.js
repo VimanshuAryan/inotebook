adfd24d1491c6d03f5a791ece63384912770bb12
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-10-07 12:48:33 +0530

    mongo db - connection

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'thanos',
-        number: 34
-    }
-    res.json(obj);
-});
+
+//create a user using: POST "/api/auth/createuser". No Login required
+ 
+router.post('/createuser',
+    body('email').isEmail(),
+    body('name').isLength({ min: 3 }),
+    body('password', 'password must be atleast 5 characters').isLength({ min: 5 }),
+    (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          }).then(user => res.json(user))
+          .catch(err => {console.log(err)
+          res.json({error: "Please enter unique value for email", message: err.message})});
+
+    });
 
 module.exports = router;
