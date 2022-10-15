7a69192587b4a6964173d9db635252457fa3b434
Vimanshu Aryan <vimanshuaryan@gmail.com>
2022-10-15 18:07:14 +0530

    useContext and useLocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 
-import { useState } from "react";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
+
 
 export default function App() {
 
   return (
- 
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about/*" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
+    </>
+
   );
 }
